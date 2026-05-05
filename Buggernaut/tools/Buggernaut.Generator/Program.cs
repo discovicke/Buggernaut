9126926639dfc672f5f9b09ac3e7ea2ef65401cc
disco <discovicke@gmail.com>
2026-05-06 00:15:45 +0200

    feat: Update prompt category to BlackBox and add additional challenge details in Program.cs

--- Buggernaut/tools/Buggernaut.Generator/Program.cs
+++ Buggernaut/tools/Buggernaut.Generator/Program.cs
@@ -18,7 +18,7 @@ class Program
         }
 
         var client = new GeminiClient(apiKey);
-        var prompt = PromptBuilder.BuildUserPrompt(ChallengeCategories.Bug, Difficulties.Easy);
+        var prompt = PromptBuilder.BuildUserPrompt(ChallengeCategories.BlackBox, Difficulties.Hard);
         var raw = await client.GenerateAsync(prompt);
 
         Console.WriteLine("=== RAW ===");
@@ -32,5 +32,11 @@ class Program
         Console.WriteLine(challenge.BuggyCode);
         Console.WriteLine("\n--- Hint ---");
         Console.WriteLine(challenge.Hint);
+        Console.WriteLine("\n--- Solution Code ---");
+        Console.WriteLine(challenge.SolutionCode);
+        Console.WriteLine("\n--- Explanation ---");
+        Console.WriteLine(challenge.Explanation);
+        Console.WriteLine("\n--- Test Code ---");
+        Console.WriteLine(challenge.TestCode);
     }
 }
