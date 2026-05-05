f47e78aafe2b43346285c8982ea978fe56f210d5
disco <discovicke@gmail.com>
2026-05-05 23:45:51 +0200

    feat: Add ChallengeParser for JSON deserialization of Challenge objects

---
+++ Buggernaut/tools/Buggernaut.Generator/Service/ChallengeParser.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+
+namespace Buggernaut.Generator;
+
+public class ChallengeParser
+{
+    public static Challenge Parse(string json)
+    {
+        return JsonSerializer.Deserialize<Challenge>(json);
+    }
+}
