03c96f76397416c95f47b4649ccbb453db9f98f3
disco <discovicke@gmail.com>
2026-05-05 23:07:16 +0200

    feat: Add challenge class

---
+++ Buggernaut/tools/Buggernaut.Generator/Challenge.cs
@@ -0,0 +1,11 @@
+﻿namespace Buggernaut.Generator;
+
+public class Challenge
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string BuggyCode { get; set; }
+    public string Hint { get; set; }
+    public string SolutionCode { get; set; }
+    public string Explanation { get; set; }
+}
