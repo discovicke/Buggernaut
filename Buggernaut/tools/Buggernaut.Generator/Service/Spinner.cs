7e1b4d84e31f98d783b5996419a79ecec70f4d67
disco <discovicke@gmail.com>
2026-05-06 17:47:20 +0200

    feat: Add Spinner class for animated loading indication during long-running operations

---
+++ Buggernaut/tools/Buggernaut.Generator/Service/Spinner.cs
@@ -0,0 +1,56 @@
+﻿namespace Buggernaut.Generator;
+
+/// <summary>
+/// Animated loading spinner for long-running operations.
+/// Use in a using-statement for automatic cleanup.
+///
+/// Example:
+///   using (var spinner = new Spinner("Väntar på API..."))
+///   {
+///       await SomeAsyncOperation();
+///   }
+/// </summary>
+public class Spinner : IDisposable
+{
+    private readonly string _message;
+    private readonly Thread _thread;
+    private bool _isSpinning;
+
+    public Spinner(string message)
+    {
+        _message = message;
+        _isSpinning = true;
+        _thread = new Thread(Spin);
+        _thread.IsBackground = true;
+        _thread.Start();
+    }
+
+    private void Spin()
+    {
+        var chars = new[] { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' };
+        int i = 0;
+
+        while (_isSpinning)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"\r[{chars[i]}] {_message}");
+            Console.ResetColor();
+            i = (i + 1) % chars.Length;
+            Thread.Sleep(100);
+        }
+    }
+
+    public void Dispose()
+    {
+        _isSpinning = false;
+        _thread.Join(TimeSpan.FromSeconds(1));
+
+        try
+        {
+            Console.Write($"\r{new string(' ', Console.WindowWidth - 1)}\r");
+        }
+        catch
+        { }
+    }
+}
+
