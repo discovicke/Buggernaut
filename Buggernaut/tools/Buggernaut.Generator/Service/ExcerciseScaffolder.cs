82cff799beaff1f5ac5ad1001279973c51431006
disco <discovicke@gmail.com>
2026-05-06 10:31:16 +0200

    feat: Add ExcerciseScaffolder

---
+++ Buggernaut/tools/Buggernaut.Generator/Service/ExcerciseScaffolder.cs
@@ -0,0 +1,25 @@
+﻿namespace Buggernaut.Generator;
+
+public class ExcerciseScaffolder(string rootPath)
+{
+    private readonly string _rootPath = rootPath;
+
+    public void Scaffold(Challenge challenge)
+    {
+        var exercisePath = Path.Combine(_rootPath, "src", "Exercises", challenge.Title);
+        var testPath = Path.Combine(_rootPath, "tests", challenge.Title);
+        var solutionPath = Path.Combine(_rootPath, "src", "Solutions", challenge.Title);
+        
+        Directory.CreateDirectory(exercisePath);
+        Directory.CreateDirectory(testPath);
+        Directory.CreateDirectory(solutionPath);
+        
+        File.WriteAllText(exercisePath, challenge.BuggyCode);
+        File.WriteAllText(testPath,     challenge.TestCode);
+        File.WriteAllText(solutionPath, challenge.SolutionCode);
+
+        Console.WriteLine($"\n Övning skapad: {challenge.Title}");
+        Console.WriteLine($"   Öppna:     src/Buggernaut.Exercises/{challenge.Title}.cs");
+        Console.WriteLine($"   Testa med: dotnet test");
+    }
+}
