8cdd6e500f5b1df8268f52a629c94d083103e581
disco <discovicke@gmail.com>
2026-05-11 16:36:06 +0200

    feat: add ConfigLoader class to manage application configuration loading

---
+++ Buggernaut/tools/Buggernaut.Generator/Service/Config/ConfigLoader.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Buggernaut.Generator;
+
+internal static class ConfigLoader
+{
+    public static IConfiguration Load()
+    {
+        var solutionRoot = SolutionRootFinder.Find();
+        var appSettingsPath = Path.Combine(solutionRoot, "tools", "Buggernaut.Generator", "appsettings.json");
+
+        return new ConfigurationBuilder()
+            .AddJsonFile(appSettingsPath, optional: true)
+            .AddUserSecrets(typeof(ConfigLoader).Assembly)
+            .Build();
+    }
+}
+
